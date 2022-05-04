2bd3424592efec375f340943549fc55aa6a79d15
JuwonCha <juwon1512@gmail.com>
2022-05-04 22:10:11 +0900

    Initial Commit

--- PracticeWindowsAPI/winmain.cpp
+++ PracticeWindowsAPI/winmain.cpp
@@ -1,2 +1,18 @@
 #include <Windows.h>
 
+int WINAPI WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPSTR lpCmdLine,
+	_In_ int nShowCmd
+)
+{
+	MessageBox(
+		nullptr,
+		L"Hello World!",
+		L"SimpleWindwo",
+		MB_ICONEXCLAMATION | MB_OKCANCEL
+	);
+
+	return 0;
+}
