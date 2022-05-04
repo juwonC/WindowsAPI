c5c45b662e6966e0f6f9d3734927f7ea46e67608
JuwonCha <juwon1512@gmail.com>
2022-05-04 22:57:25 +0900

    Add Window Class, Window Procedure

--- PracticeWindowsAPI/winmain.cpp
+++ PracticeWindowsAPI/winmain.cpp
@@ -1,5 +1,9 @@
 #include <Windows.h>
 
+const wchar_t gClassName[] = L"MyWindowClass";
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+
 int WINAPI WinMain(
 	_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
@@ -7,12 +11,88 @@ int WINAPI WinMain(
 	_In_ int nShowCmd
 )
 {
-	MessageBox(
-		nullptr,
-		L"Hello World!",
-		L"SimpleWindwo",
-		MB_ICONEXCLAMATION | MB_OKCANCEL
+	//MessageBox(
+	//	nullptr,
+	//	L"Hello World!",
+	//	L"SimpleWindwo",
+	//	MB_ICONEXCLAMATION | MB_OKCANCEL
+	//);
+	
+
+	// Window Class
+	WNDCLASSEX wc;
+
+	ZeroMemory(&wc, sizeof(WNDCLASSEX));
+	wc.cbSize = sizeof(WNDCLASSEX);
+
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpszClassName = gClassName;
+	wc.hInstance = hInstance;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.lpfnWndProc = WindowProc;
+
+	// Register a Window Class
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBox(nullptr, L"Failed to Register Window Class!", L"Error", MB_OK);
+		return 0;
+	}
+
+	// Create a Window
+	HWND hwnd;
+
+	hwnd = CreateWindowEx(
+		0,
+		gClassName,
+		L"SimpleWindow APP",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		640,
+		480,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
 	);
 
+	if (!hwnd)
+	{
+		MessageBox(nullptr, L"Failed To Create Window!", L"Error", MB_OK);
+		return 0;
+	}
+
+	ShowWindow(hwnd, nShowCmd);
+	UpdateWindow(hwnd);
+
+	// Window Message Loop
+	MSG msg;
+	while (GetMessage(&msg, 0, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessageW(&msg);
+	}
+
+	return (int)msg.wParam;
+}
+
+// Write the Window Procedure
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+		case WM_CLOSE:
+			DestroyWindow(hwnd);
+			break;
+
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			break;
+
+		default:
+			return DefWindowProc(hwnd, message, wParam, lParam);
+	}
+	
 	return 0;
 }
