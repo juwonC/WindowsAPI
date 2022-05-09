e9ab48a1fea9914492b78d97c8eb0f9ae1e50300
JuwonCha <juwon1512@gmail.com>
2022-05-09 22:31:07 +0900

    Refactor Window Procedure

--- MyWindow/main.cpp
+++ MyWindow/main.cpp
@@ -3,18 +3,25 @@
 // 3. 윈도우 메시지 루프 처리
 // 4. 윈도우 프로시져(Procedure) 작성
 
-#include <Windows.h>
+#include <windows.h>
 #include <sstream>
+
+const wchar_t ClassName[] = L"Window Class";
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow)
+int WINAPI WinMain(
+	_In_ HINSTANCE hInstance, 
+	_In_opt_ HINSTANCE hPrevInstance, 
+	_In_ LPSTR lpCmdLine, 
+	_In_ int nShowCmd
+)
 {
-	const wchar_t MyWindow[] = L"Window Class";
-	
-	MSG msg;
 	BOOL bRet;
-	WNDCLASS wc;
+	WNDCLASSEX wc{};
+
+	ZeroMemory(&wc, sizeof(WNDCLASSEX));
+	wc.cbSize = sizeof(WNDCLASSEX);
 
 	wc.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
 	wc.lpfnWndProc = (WNDPROC)WndProc;
@@ -25,17 +32,17 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine,
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
 	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
 	wc.lpszMenuName = L"MyWindow";
-	wc.lpszClassName = MyWindow;
+	wc.lpszClassName = ClassName;
 
-	RegisterClass(&wc);
+	RegisterClassEx(&wc);
 	
 	HWND hwnd = CreateWindowEx(
 		0, 
-		MyWindow, 
+		ClassName,
 		L"Mywindow", 
 		WS_OVERLAPPEDWINDOW | WS_VSCROLL,
 		CW_USEDEFAULT,
-		CW_USEDEFAULT, 
+		CW_USEDEFAULT,
 		CW_USEDEFAULT,
 		CW_USEDEFAULT,
 		NULL,
@@ -44,9 +51,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine,
 		NULL
 		);
 
-	ShowWindow(hwnd, nCmdShow);
+	ShowWindow(hwnd, nShowCmd);
+	UpdateWindow(hwnd);
 
 	// Window Message Loop
+	MSG msg;
+
 	while ((bRet = GetMessage(&msg, NULL, 0, 0)) != 0)
 	{
 		if (bRet == -1)
@@ -56,17 +66,57 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine,
 		else
 		{
 			TranslateMessage(&msg);
-			DispatchMessage(&msg);
+			DispatchMessageW(&msg);
 		}
 	}
 
 	return 0;
 }
 
+void OnPaint(HWND hwnd)
+{
+	PAINTSTRUCT ps;
+	HDC hdc = BeginPaint(hwnd, &ps);
+
+	HPEN redPen = CreatePen(PS_DASH, 1, RGB(255, 0, 0));
+	HPEN oldPen = (HPEN)SelectObject(hdc, redPen);
+	
+	Ellipse(hdc, 0, 0, 100, 100);
+
+	DeleteObject(redPen);
+	SelectObject(hdc, oldPen);
+	
+	HBRUSH brush = CreateSolidBrush(RGB(0, 0, 255));
+	HBRUSH oldBrush = (HBRUSH)SelectObject(hdc, brush);
+
+	Rectangle(hdc, 100, 100, 0, 200);
+	RoundRect(hdc, 150, 150, 200, 200, 10, 50);
+
+	DeleteObject(brush);
+	SelectObject(hdc, oldBrush);
+
+	EndPaint(hwnd, &ps);
+}
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	switch (message)
 	{
+		case WM_KEYDOWN:
+		{
+			std::wostringstream oss;
+			
+			oss << wParam << " " << lParam << std::endl;
+			MessageBox(hwnd, oss.str().c_str(), L"Keyboard Input", MB_OK);
+
+			if (wParam == VK_DOWN)
+			{
+				OutputDebugStringW(oss.str().c_str());
+			}
+
+			break;
+		}
+		
 		case WM_LBUTTONDOWN:
 		{
 			POINT pt;
@@ -76,23 +126,25 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 			std::wostringstream oss;
 
-			oss << "X : " << pt.x
-				<< ", Y : " << pt.y;
+			oss << "X : " << pt.x << ", Y : " << pt.y << std::endl;
 
-			MessageBox(hwnd, oss.str().c_str(), L"Coordinate", MB_OK);
+			//MessageBox(hwnd, oss.str().c_str(), L"Coordinate", MB_OK);
+
+			OutputDebugStringW(oss.str().c_str());
 
 			break;
 		}
 
 		case WM_PAINT:
-		{
-			PAINTSTRUCT ps;
-			HDC hdc = BeginPaint(hwnd, &ps);
-
-			Rectangle(hdc, 0, 0, 100, 100);
-
+			OnPaint(hwnd);
 			break;
-		}
+
+		case WM_CLOSE:
+			if (MessageBox(hwnd, L"Really quit?", L"My Winodw", MB_OKCANCEL) == IDOK)
+			{
+				DestroyWindow(hwnd);
+			}
+			break;
 
 		case WM_DESTROY:
 			PostQuitMessage(0);
