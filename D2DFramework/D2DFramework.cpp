38ae37ff51eb7a739328af88bc3d00741c30af72
JuwonCha <juwon1512@gmail.com>
2022-05-27 21:11:37 +0900

    Add com_exception Class

--- D2DFramework/D2DFramework.cpp
+++ D2DFramework/D2DFramework.cpp
@@ -9,11 +9,12 @@ HRESULT D2DFramework::Init(HWND hwnd)
 		D2D1_FACTORY_TYPE_SINGLE_THREADED,
 		mspD2DFactory.GetAddressOf()
 	);
-	if (FAILED(hr))
-	{
-		ShowErrorMsg(L"Failed to create the D2D Factory");
-		return hr;
-	}
+	//if (FAILED(hr))
+	//{
+	//	ShowErrorMsg(L"Failed to create the D2D Factory");
+	//	return hr;
+	//}
+	ThrowIfFailed(hr);
 
 	RECT rc;
 	GetClientRect(hwnd, &rc);
--- D2DFramework/D2DFramework.h
+++ D2DFramework/D2DFramework.h
@@ -2,6 +2,35 @@
 
 #include <d2d1.h>
 #include <wrl/client.h>
+#include <exception>
+#include <sstream>
+
+class com_exception : public std::exception
+{
+private:
+	HRESULT mResult;
+
+public:
+	com_exception(HRESULT hr) : mResult(hr) {}
+
+	virtual const char* what() const override
+	{
+		std::stringstream ss;
+		ss << "Failure with Code : "
+			<< mResult << std::endl;
+
+		return ss.str().c_str();
+	}
+
+};
+
+inline void ThrowIfFailed(HRESULT hr)
+{
+	if (FAILED(hr))
+	{
+		throw com_exception(hr);
+	}
+}
 
 class D2DFramework
 {
